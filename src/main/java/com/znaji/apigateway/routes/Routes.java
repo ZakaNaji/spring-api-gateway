b37bb3ed1e718e4b2ce5fec0e561985eeefc9e9d
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-06-13 23:31:16 +0100

    #02: add product service route

--- src/main/java/com/znaji/apigateway/routes/Routes.java
+++ src/main/java/com/znaji/apigateway/routes/Routes.java
@@ -2,6 +2,8 @@ package com.znaji.apigateway.routes;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicate;
+import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
@@ -13,8 +15,7 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
         return route("product-service")
-                .route(r -> r.path().equals("/api/products"), http("http://localhost:8080"))
+                .route(RequestPredicates.path("/api/products/**"), http("http://localhost:8080"))
                 .build();
-
     }
 }
