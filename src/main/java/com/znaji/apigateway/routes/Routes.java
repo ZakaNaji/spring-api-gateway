1e28722061843a2718c58c936a4fe2c0b6c03d53
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-06-22 12:35:16 +0100

    #06-2: circuitBreaker configuration.

--- src/main/java/com/znaji/apigateway/routes/Routes.java
+++ src/main/java/com/znaji/apigateway/routes/Routes.java
@@ -1,11 +1,15 @@
 package com.znaji.apigateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicate;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
@@ -17,6 +21,8 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceRoute() {
         return route("product-service")
                 .route(RequestPredicates.path("/api/products/**"), http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("product-service-circuit-breaker",
+                        URI.create("forward:/fallback")))
                 .build();
     }
 
@@ -55,6 +61,15 @@ public class Routes {
         return route("inventory-service-swagger")
                 .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), http("http://localhost:8082"))
                 .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fullBackRoute() {
+        return route("full-back")
+                .GET("/fallback",
+                        response -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Service is temporarily unavailable"))
                 .build();
     }
 }
